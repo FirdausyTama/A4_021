eeb48f5b0c2557468049ff6489be3fb998b4e8c1
FirdausyTama <atama.firdausy@gmail.com>
2025-01-18 17:15:32 +0700

    membuat data model properti

---
+++ app/src/main/java/com/example/propertiapp/model/Properti.kt
@@ -0,0 +1,31 @@
+package com.example.propertiapp.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Properti(
+    @SerialName("id_properti")
+    val idProperti: String,
+
+    @SerialName("nama_properti")
+    val namaProperti: String,
+
+    @SerialName("deskripsi_properti")
+    val deskripsiProperti: String,
+
+    val lokasi: String,
+    val harga: String,
+
+    @SerialName("status_properti")
+    val statusProperti: String,
+
+    @SerialName("id_jenis")
+    val idJenis: String,
+
+    @SerialName("id_pemilik")
+    val idPemilik: String,
+
+    @SerialName("id_manajer")
+    val idManajer: String
+)

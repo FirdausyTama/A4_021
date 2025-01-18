bb854e5ee37a3b5673c3ea2d991d5e252bad18b3
FirdausyTama <atama.firdausy@gmail.com>
2025-01-18 19:34:47 +0700

    membuat homepropertiVM(viewmodel)

---
+++ app/src/main/java/com/example/propertiapp/ui/viewmodel/properti/HomePropertiVM.kt
@@ -0,0 +1,52 @@
+package com.example.propertiapp.ui.viewmodel.properti
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import coil.network.HttpException
+import com.example.propertiapp.model.Properti
+import com.example.propertiapp.repository.PropertiRepository
+import kotlinx.coroutines.launch
+import okio.IOException
+
+sealed class HomeUiState {
+    data class Success(val properti: List<Properti>): HomeUiState()
+    object Error: HomeUiState()
+    object Loading: HomeUiState()
+}
+
+class HomeViewModel(private val propertiRepo: PropertiRepository): ViewModel() {
+    var propertiUiState: HomeUiState by mutableStateOf(HomeUiState.Loading)
+        private set
+
+    init {
+        getProperti()
+    }
+
+    fun getProperti() {
+        viewModelScope.launch {
+            propertiUiState = HomeUiState.Loading
+            propertiUiState = try {
+                HomeUiState.Success(propertiRepo.getProperti())
+            } catch (e: IOException) {
+                HomeUiState.Error
+            } catch (e: HttpException) {
+                HomeUiState.Error
+            }
+        }
+    }
+
+    fun deleteProperti(idProperti: String) {
+        viewModelScope.launch {
+            try {
+                propertiRepo.deleteProperti(idProperti)
+            } catch (e: IOException) {
+                HomeUiState.Error
+            } catch (e: HttpException) {
+                HomeUiState.Error
+            }
+        }
+    }
+}

de27a6a750cbbc4f95b4c45fe87f5cfe34c68417
FirdausyTama <atama.firdausy@gmail.com>
2025-01-18 17:20:10 +0700

    membuat properti service sambung API

---
+++ app/src/main/java/com/example/propertiapp/service/PropertiService.kt
@@ -0,0 +1,36 @@
+package com.example.propertiapp.service
+
+import com.example.propertiapp.model.Properti
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Query
+
+annotation class leaders
+
+interface PropertiService {
+
+    @Headers(
+        "Accept: application/json",
+        "Content-Type: application/json",
+    )
+
+    @POST("insertproperti.php")
+    suspend fun insertProperti(@Body properti: Properti)
+
+    @GET("bacaproperti.php")
+    suspend fun getAllProperti(): List<Properti>
+
+    @GET("baca1properti.php/{id_properti}")
+    suspend fun getPropertiById(@Query("id_properti") id_properti: String): Properti
+
+    @PUT("editproperti.php/{id_properti}")
+    suspend fun updateProperti(@Query("id_properti") id_properti: String, @Body properti: Properti)
+
+    @DELETE("deleteproperti.php/{id_properti}")
+    suspend fun deleteProperti(@Query("id_properti") id_properti: String): Response<Void>
+}

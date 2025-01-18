01f0d2fc221b0b2c031b5ae57c97aecb49346e45
FirdausyTama <atama.firdausy@gmail.com>
2025-01-18 20:17:30 +0700

    membuat penyedia videw model

--- app/src/main/java/com/example/propertiapp/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/propertiapp/ui/viewmodel/PenyediaViewModel.kt
@@ -1,2 +1,17 @@
 package com.example.propertiapp.ui.viewmodel
 
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.propertiapp.PropertiApplication
+import com.example.propertiapp.ui.viewmodel.properti.HomeViewModel
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer { HomeViewModel(aplikasiKontak().container.propertiRepository) }
+    }
+}
+
+fun CreationExtras.aplikasiKontak():PropertiApplication =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY]as PropertiApplication)

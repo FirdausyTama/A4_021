2b5572bced4c75b2bbd91a8859e930436881439b
FirdausyTama <atama.firdausy@gmail.com>
2025-01-19 13:57:04 +0700

    membuat pengelola halaman detail

--- app/src/main/java/com/example/propertiapp/navigasi/PengelolaHalaman.kt
+++ app/src/main/java/com/example/propertiapp/navigasi/PengelolaHalaman.kt
@@ -3,13 +3,17 @@ package com.example.propertiapp.navigasi
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.propertiapp.ui.view.properti.DestinasiHome
 import com.example.propertiapp.ui.view.properti.HomeScreen
 import com.example.propertiapp.ui.view.properti.DestinasiEntry
 import com.example.propertiapp.ui.view.properti.EntryPropertiScreen
+import com.example.propertiapp.ui.view.properti.DestinasiDetailProperti
+import com.example.propertiapp.ui.view.properti.DetailPropertiScreen
 
 @Composable
 fun PengelolaHalaman(navController: NavHostController = rememberNavController()) {
@@ -22,8 +26,8 @@ fun PengelolaHalaman(navController: NavHostController = rememberNavController())
         composable(DestinasiHome.route) {
             HomeScreen(
                 navigateToItemEntry = { navController.navigate(DestinasiEntry.route)},
-                onDetailClick = { /*idProperti ->
-                    navController.navigate("${DestinasiDetail.route}/$idProperti")*/
+                onDetailClick = { idProperti ->
+                    navController.navigate("${DestinasiDetailProperti.route}/$idProperti")
                 }
             )
         }
@@ -32,6 +36,21 @@ fun PengelolaHalaman(navController: NavHostController = rememberNavController())
         composable(DestinasiEntry.route) {
             EntryPropertiScreen(
                 navigateBack = { navController.navigate(DestinasiHome.route) }
+            )
+        }
+
+        // Detail Screen
+        composable(
+            route = "${DestinasiDetailProperti.route}/{idProperti}",
+            arguments = listOf(navArgument("idProperti") { type = NavType.StringType })
+        ) { backStackEntry ->
+            val idProperti = backStackEntry.arguments?.getString("idProperti") ?: ""
+            DetailPropertiScreen(
+                idProperti = idProperti,
+                onNavigateBack = { navController.navigateUp() },
+                onEditClick = {
+                    navController.navigate("${DestinasiEntry.route}?idProperti=$idProperti")
+                }
             )
         }
     }

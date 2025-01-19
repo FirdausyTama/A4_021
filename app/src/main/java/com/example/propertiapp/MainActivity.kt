ed324efc17cb1c0a9b524a2c8b97c1c253f07c3e
FirdausyTama <atama.firdausy@gmail.com>
2025-01-19 11:09:40 +0700

    memanggil propertiApp di MainActivity

--- app/src/main/java/com/example/propertiapp/MainActivity.kt
+++ app/src/main/java/com/example/propertiapp/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.propertiapp.ui.theme.PropertiAppTheme
+import com.example.propertiapp.ui.viewmodel.PropertiApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,8 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             PropertiAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    PropertiApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }

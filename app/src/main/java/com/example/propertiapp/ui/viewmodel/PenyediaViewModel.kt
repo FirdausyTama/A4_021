834417d009332dcf981871985e711981a7e5aff3
FirdausyTama <atama.firdausy@gmail.com>
2025-01-20 20:57:46 +0700

    menambah jenisviewmodel ke penyedia

--- app/src/main/java/com/example/propertiapp/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/propertiapp/ui/viewmodel/PenyediaViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.propertiapp.PropertiApplication
+import com.example.propertiapp.ui.viewmodel.jenisproperti.JenisViewModel
 import com.example.propertiapp.ui.viewmodel.properti.DetailPropertiViewModel
 import com.example.propertiapp.ui.viewmodel.properti.HomeViewModel
 import com.example.propertiapp.ui.viewmodel.properti.InsertViewModel
@@ -14,6 +15,7 @@ object PenyediaViewModel {
         initializer { HomeViewModel(aplikasiProperti().container.propertiRepository) }
         initializer { InsertViewModel(aplikasiProperti().container.propertiRepository) }
         initializer { DetailPropertiViewModel(aplikasiProperti().container.propertiRepository) }
+        initializer { JenisViewModel(aplikasiProperti().container.jenisRepository) }
     }
 }
 

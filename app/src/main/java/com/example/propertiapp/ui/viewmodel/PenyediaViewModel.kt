297fc8b756440439629b3ba4a9d22dc207f45b22
FirdausyTama <atama.firdausy@gmail.com>
2025-01-22 00:27:43 +0700

    mendaftarkan ke penyedia

--- app/src/main/java/com/example/propertiapp/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/propertiapp/ui/viewmodel/PenyediaViewModel.kt
@@ -8,18 +8,28 @@ import com.example.propertiapp.PropertiApplication
 import com.example.propertiapp.ui.viewmodel.jenisproperti.DetailJenisViewModel
 import com.example.propertiapp.ui.viewmodel.jenisproperti.InsertJenisViewModel
 import com.example.propertiapp.ui.viewmodel.jenisproperti.JenisViewModel
+import com.example.propertiapp.ui.viewmodel.pemilik.HomePemilikVM
+import com.example.propertiapp.ui.viewmodel.pemilik.InsertPemilikViewModel
 import com.example.propertiapp.ui.viewmodel.properti.DetailPropertiViewModel
 import com.example.propertiapp.ui.viewmodel.properti.HomeViewModel
 import com.example.propertiapp.ui.viewmodel.properti.InsertViewModel
+import com.example.propertiapp.ui.viewmodel.properti.UpdatePropertiViewModel
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer { HomeViewModel(aplikasiProperti().container.propertiRepository) }
         initializer { InsertViewModel(aplikasiProperti().container.propertiRepository) }
         initializer { DetailPropertiViewModel(aplikasiProperti().container.propertiRepository) }
+        initializer { UpdatePropertiViewModel(aplikasiProperti().container.propertiRepository) }
+
+        //jenis
         initializer { JenisViewModel(aplikasiProperti().container.jenisRepository) }
         initializer { InsertJenisViewModel(aplikasiProperti().container.jenisRepository) }
         initializer { DetailJenisViewModel(aplikasiProperti().container.jenisRepository) }
+
+        //Pemilik
+        initializer { HomePemilikVM(aplikasiProperti().container.pemilikRepository) }
+        initializer { InsertPemilikViewModel(aplikasiProperti().container.pemilikRepository) }
     }
 }
 

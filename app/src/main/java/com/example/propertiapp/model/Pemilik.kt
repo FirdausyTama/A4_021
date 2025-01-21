063c765f5dbc1b033672fb68f4733534afb519c3
FirdausyTama <atama.firdausy@gmail.com>
2025-01-21 22:50:52 +0700

    membuat model pemilik

---
+++ app/src/main/java/com/example/propertiapp/model/Pemilik.kt
@@ -0,0 +1,16 @@
+package com.example.propertiapp.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Pemilik(
+    @SerialName("id_pemilik")
+    val idPemilik: String,
+
+    @SerialName("nama_pemilik")
+    val namaPemilik: String,
+
+    @SerialName("kontak_pemilik")
+    val kontakPemilik: String
+)

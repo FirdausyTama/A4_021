0aa211cebc9b846b1b5c860f02de34c34facc792
FirdausyTama <atama.firdausy@gmail.com>
2025-01-22 00:25:03 +0700

    update properti container

--- app/src/main/java/com/example/propertiapp/di/PropertiContainer.kt
+++ app/src/main/java/com/example/propertiapp/di/PropertiContainer.kt
@@ -4,6 +4,9 @@ import com.example.propertiapp.repository.PropertiRepository
 import com.example.propertiapp.repository.NetworkPropertiRepository
 import com.example.propertiapp.repository.JenisRepository
 import com.example.propertiapp.repository.NetworkJenisRepository
+import com.example.propertiapp.repository.PemilikRepository
+import com.example.propertiapp.repository.NetworkPemilikRepository
+import com.example.propertiapp.service.PemilikService
 import com.example.propertiapp.service.PropertiService
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
@@ -13,10 +16,10 @@ import retrofit2.Retrofit
 interface AppContainer {
     val propertiRepository: PropertiRepository
     val jenisRepository: JenisRepository
+    val pemilikRepository: PemilikRepository
 }
 
 class PropertiContainer : AppContainer {
-
     private val baseUrl = "http://10.0.2.2:80/pamTI/"
     private val json = Json { ignoreUnknownKeys = true }
     private val retrofit: Retrofit = Retrofit.Builder()
@@ -28,11 +31,19 @@ class PropertiContainer : AppContainer {
         retrofit.create(PropertiService::class.java)
     }
 
+    private val pemilikService: PemilikService by lazy {
+        retrofit.create(PemilikService::class.java)
+    }
+
     override val propertiRepository: PropertiRepository by lazy {
         NetworkPropertiRepository(propertiService)
     }
 
     override val jenisRepository: JenisRepository by lazy {
         NetworkJenisRepository(propertiService)
+    }
+
+    override val pemilikRepository: PemilikRepository by lazy {
+        NetworkPemilikRepository(pemilikService)
     }
 }

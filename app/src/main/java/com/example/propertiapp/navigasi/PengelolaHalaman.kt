bcaf50be1a6b3278fc8d64407ab876cf69bc0829
FirdausyTama <atama.firdausy@gmail.com>
2025-01-22 00:28:13 +0700

    membuat destinasi navigasi di pengelola

--- app/src/main/java/com/example/propertiapp/navigasi/PengelolaHalaman.kt
+++ app/src/main/java/com/example/propertiapp/navigasi/PengelolaHalaman.kt
@@ -8,40 +8,78 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import com.example.propertiapp.ui.view.DestinasiHomeStart
+import com.example.propertiapp.ui.view.HomeStartScreen
 import com.example.propertiapp.ui.view.properti.*
 import com.example.propertiapp.ui.view.jenis.*
 import com.example.propertiapp.ui.view.jenisproperti.DestinasiDetailJenis
 import com.example.propertiapp.ui.view.jenisproperti.DestinasiEntryJenis
 import com.example.propertiapp.ui.view.jenisproperti.DetailJenisScreen
 import com.example.propertiapp.ui.view.jenisproperti.EntryJenisScreen
+import com.example.propertiapp.ui.view.pemilik.DestinasiEntryPemilik
+import com.example.propertiapp.ui.view.pemilik.DestinasiPemilik
+import com.example.propertiapp.ui.view.pemilik.InsertPemilikView
+import com.example.propertiapp.ui.view.pemilik.PemilikScreen
 
 @Composable
 fun PengelolaHalaman(navController: NavHostController = rememberNavController()) {
     NavHost(
         navController = navController,
-        startDestination = DestinasiHome.route,
+        startDestination = DestinasiHomeStart.route,  // Ubah start destination
         modifier = Modifier,
     ) {
+        // Home Start Screen
+        composable(DestinasiHomeStart.route) {
+            HomeStartScreen(
+                navigateToHome = {
+                    navController.navigate(DestinasiHome.route) {
+                        popUpTo(DestinasiHomeStart.route) { inclusive = true }
+                    }
+                }
+            )
+        }
         // Properti Home
         composable(DestinasiHome.route) {
             HomeScreen(
                 navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
                 navigateToJenis = { navController.navigate(DestinasiJenis.route) },
-                navigateToPemilik = {}, // Tambahkan navigasi ke Settings
-                navigateToManajer = {}, // Tambahkan navigasi ke About
+                navigateToPemilik = { navController.navigate(DestinasiPemilik.route) },
+                navigateToManajer = {},
                 onDetailClick = { idProperti ->
                     navController.navigate("${DestinasiDetailProperti.route}/$idProperti")
                 }
             )
         }
 
+        // Pemilik Screen
+        composable(DestinasiPemilik.route) {
+            PemilikScreen(
+                navigateToItemEntry = { navController.navigate(DestinasiEntryPemilik.route) },
+                navigateBack = { navController.navigateUp() },
+                onDetailClick = { /*idPemilik ->
+                    navController.navigate("${DestinasiDetailPemilik.route}/$idPemilik")*/
+                }
+            )
+        }
 
+        // Entry Pemilik Screen
+        composable(DestinasiEntryPemilik.route) {
+            InsertPemilikView(
+                navigateBack = { navController.navigateUp() },
+                onNavigateBack = {
+                    navController.navigate(DestinasiPemilik.route) {
+                        popUpTo(DestinasiPemilik.route) { inclusive = true }
+                    }
+                }
+            )
+        }
+
+        // Properti Entry
         composable(DestinasiEntry.route) {
             EntryPropertiScreen(
                 navigateBack = { navController.navigate(DestinasiHome.route) }
             )
         }
-
 
         // Detail properti
         composable(
@@ -59,7 +97,7 @@ fun PengelolaHalaman(navController: NavHostController = rememberNavController())
             )
         }
 
-        // Navigasi untuk Update Properti
+        // Edit Properti
         composable(
             route = "${DestinasiEditProperti.route}/{idProperti}",
             arguments = listOf(navArgument("idProperti") { type = NavType.StringType })

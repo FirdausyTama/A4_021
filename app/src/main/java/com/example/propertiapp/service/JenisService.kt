43789e26e788e557d8313db311765b74e75b3187
FirdausyTama <atama.firdausy@gmail.com>
2025-01-25 21:55:34 +0700

    revisi pindah file jenis service

---
+++ app/src/main/java/com/example/propertiapp/service/JenisService.kt
@@ -0,0 +1,39 @@
+package com.example.propertiapp.service
+
+
+import com.example.propertiapp.model.Properti
+import com.example.propertiapp.model.Jenis
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Query
+
+annotation class leadersssz
+
+interface JenisService {
+
+    @Headers(
+        "Accept: application/json",
+        "Content-Type: application/json",
+    )
+
+    // Jenis endpoints
+    @POST("insertjenis.php")
+    suspend fun insertJenis(@Body jenis: Jenis)
+
+    @GET("bacajenis.php")
+    suspend fun getAllJenis(): List<Jenis>
+
+    @GET("baca1jenis.php")
+    suspend fun getJenisById(@Query("id_jenis") idJenis: String): Jenis
+
+    @PUT("editjenis.php/{id_jenis}")
+    suspend fun updateJenis(@Query("id_jenis") idJenis: String, @Body jenis: Jenis)
+
+    @DELETE("deletejenis.php")
+    suspend fun deleteJenis(@Query("id_jenis") idJenis: String): Response<Void>
+}
